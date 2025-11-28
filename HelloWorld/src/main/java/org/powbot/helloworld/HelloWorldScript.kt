445ffb510eb63a9f43a80c7c4d54d4a78c79584b
constt <ijavaservices@gmail.com>
2025-11-29 07:21:54 +1300

    Add version to ScriptManifest annotation

--- HelloWorld/src/main/java/org/powbot/helloworld/HelloWorldScript.kt
+++ HelloWorld/src/main/java/org/powbot/helloworld/HelloWorldScript.kt
@@ -4,7 +4,7 @@ import org.powbot.api.Notifications
 import org.powbot.api.script.AbstractScript
 import org.powbot.api.script.ScriptManifest
 
-@ScriptManifest(scriptId = "3374639d-9dfd-443a-994a-9cb277eebf81")
+@ScriptManifest(scriptId = "3374639d-9dfd-443a-994a-9cb277eebf81", version = "0.0.1")
 class HelloWorldScript : AbstractScript() {
 
     override fun onStart() {
