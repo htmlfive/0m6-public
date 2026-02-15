0396831e7691f44ef0125559691239a1d29b5a97
Shawn <shawn.khaan@gmail.com>
2026-02-15 15:24:19 -0500

    Add pohcake community script

---
+++ pohcake/pohcake/README.md
@@ -0,0 +1,7 @@
+# Pohcake
+
+Standalone PowBot script project for pohcake.
+
+## Build
+
+.\gradlew.bat jar
---
+++ pohcake/pohcake/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java")
+    kotlin("jvm")
+}
+
+group = "org.powbot.community.pohcake"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.powbot.org/releases")
+}
+
+dependencies {
+    implementation("org.powbot:client-sdk:3+")
+    implementation("org.powbot:client-sdk-loader:3+")
+    implementation("com.google.guava:guava:31.1-jre")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+tasks.jar {
+    archiveBaseName.set("Pohcake")
+}
